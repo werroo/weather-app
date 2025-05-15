63a3fba145f08562f7c5c81707f570235e7b9fb1
Weronika <weronika_dabrowska@yahoo.co.uk>
2025-05-15 18:36:10 +0100

    Updated JS code to create a search engine on the app

--- src/index.js
+++ src/index.js
@@ -15,12 +15,23 @@ let minutes = now.getMinutes();
 let currentTime = document.querySelector(".current-time");
 currentTime.innerHTML = `${day} ${hour}:${minutes}`;
 
+function showTemperature(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  let cityName = document.querySelector("#city-name");
+  let currentTemperature = document.querySelector(".current-value");
+  currentTemperature.innerHTML = `${temperature}`;
+  cityName.innerHTML = response.data.city;
+}
+
 function changeName(event) {
   event.preventDefault();
-  let changeCityName = document.querySelector("#city");
 
-  let cityName = document.querySelector("#city-name");
-  cityName.innerHTML = `${changeCityName.value}`;
+  let searchOutputElement = document.querySelector("#city");
+
+  let city = searchOutputElement.value;
+  let apiKey = `04b5fc5oc9b0fe6f93atfbe5cc26da36`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
 }
 
 let searchForm = document.querySelector("#search-form");
